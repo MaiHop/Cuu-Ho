0d08a6c8a3ea966bd136a24a209f93f0706002ed
MaiHop <maihop456@gmail.com>
2024-10-25 20:39:24 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //MapView
+    implementation ("org.osmdroid:osmdroid-android:6.1.12")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
 }
--- app/src/main/java/com/example/cuu_ho/MainActivity.java
+++ app/src/main/java/com/example/cuu_ho/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.cuu_ho;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.cuu_ho.Persentation.Activity.Intro.LoadingActivity;
+import com.example.cuu_ho.Persentation.Activity.Intro.WelcomeActivity;
+import com.example.cuu_ho.Persentation.Fragment.AccountFragment;
+import com.example.cuu_ho.Persentation.Fragment.HistoryFragment;
+import com.example.cuu_ho.Persentation.Fragment.HomeFragment;
+import com.example.cuu_ho.Persentation.Fragment.WorkShopFragment;
+import com.example.cuu_ho.databinding.ActivityLoadingBinding;
+import com.example.cuu_ho.databinding.ActivityLoginBinding;
+import com.example.cuu_ho.databinding.ActivityMainBinding;
+import com.example.cuu_ho.databinding.ActivityRegisterBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityLoadingBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityLoadingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
